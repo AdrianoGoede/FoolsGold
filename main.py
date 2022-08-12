5c8bf2de411d2ad867dd32bca8e9d7229f4df724
Adriano <adrianogoede@live.com>
2022-08-11 23:20:26 -0300

    Blockchain working with dummy challenge, basic endpoints implemented

---
+++ main.py
@@ -0,0 +1,30 @@
+import datetime
+from src.blockchain import Blockchain
+from flask import Flask, jsonify, request;
+
+app = Flask(__name__)
+blockchain = Blockchain()
+
+@app.route('/MineBlock', methods=['POST'])
+def new_block():
+    previous_nonce = blockchain.last_block()['nonce']
+    proof = blockchain.challenge(previous_nonce)
+    data = request.data.strip().decode()
+    if len(data) == 0:
+        return 'No data supplied!', 400
+    block = blockchain.create_block(proof, data)
+    return jsonify(block), 201
+
+@app.route('/GetChain', methods=['GET'])
+def get_blockchain():
+    return jsonify(blockchain.chain), 200
+
+@app.route('/GetChain/<index>', methods=['GET'])
+def get_block(index):
+    return jsonify([block for block in blockchain.chain if block['index'] == int(index)]), 200
+
+@app.route('/Status', methods=['GET'])
+def is_valid():
+    return jsonify({'CurrentSize': len(blockchain.chain), 'Timestamp': datetime.datetime.now(), 'ValidState': blockchain.is_chain_valid()}), 200
+
+app.run('localhost', 6666)
---
+++ src/blockchain.py
@@ -0,0 +1,58 @@
+import datetime
+from hashlib import sha256 as hash_function
+
+LEADING_ZEROES = 5
+HASH_SIZE_BYTES = 64
+
+class Blockchain:
+    
+    def __init__(self) -> None:
+        self.chain = []
+        self.create_block(nonce = 1)
+
+    def create_block(self, nonce: int, data: str = 'Genesis Block') -> dict:
+        block = {'index': len(self.chain), 
+                 'nonce': nonce,
+                 'timestamp': datetime.datetime.now(),
+                 'data': data,
+                 'previous_hash': self.last_block().get('hash') if len(self.chain) > 0 else ('0' * HASH_SIZE_BYTES)}
+        block['hash'] = self.hash_item(block)
+        self.chain.append(block)
+        return block
+
+    def last_block(self) -> dict:
+        return self.chain[-1]if len(self.chain) > 0 else None
+
+    def challenge(self, previous_nonce) -> int:
+        current = 1
+        while True:
+            operation = self.hash_item((current ** 2) - (previous_nonce ** 2))
+            if operation.startswith('0' * LEADING_ZEROES):
+                return current
+            current += 1
+
+    def is_proof_valid(self, block: dict, previous_nonce: int) -> bool:
+        return self.hash_item((block['nonce'] ** 2) - (previous_nonce ** 2)).startswith('000')
+
+    def hash_item(self, item) -> str:
+        return hash_function(str(item).encode()).hexdigest()
+
+    def is_chain_valid(self) -> bool:
+        current_index = 0
+        prev_nonce = 1
+        prev_hash = ('0' * 64)
+        for block in self.chain:
+            if block.get('index') != current_index:
+                return False
+            if block.get('previous_hash') != prev_hash:
+                return False
+            copy = dict.copy(block)
+            copy.pop('hash')
+            if self.hash_item(copy) != block['hash']:
+                return False
+            if not self.is_proof_valid(block, prev_nonce) and block['index'] != 0:
+                return False
+            current_index += 1
+            prev_nonce = block['nonce']
+            prev_hash = block.get('hash')
+        return True
